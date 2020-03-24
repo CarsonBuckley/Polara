d5e1d5bbafaf42ba69cfd8ba323fb8d32ed70263
Carson <carsonbuckley808@gmail.com>
2020-03-24 10:14:11 -0600

    Removed API Keys from public view

--- Polara/Polara/Models/Resort.swift
+++ Polara/Polara/Models/Resort.swift
@@ -10,14 +10,6 @@ import Foundation
 
 struct Resort {
     
-    //accuweather.com <--------------------- Better API then openweather
-    //http://dataservice.accuweather.com/currentconditions/v1/{location_key}
-    //API Key: OQjawqq2DGGmIeopDs9maDXZrntqhL7i
-    
-    //openweathermap.org
-    //http://api.openweathermap.org/data/2.5/weather?lat=40&lon=111&units=imperial&APPID=a3e27dbfd845c20f7ea98fc63be1300d
-    //______________________________________________[Coordinates ↑]______________________________________________________
-    
     var name: String
     var location: String
     var temperature: Int
